33b69b9f3a2e4676ca9610ceccc4446c18367e50
adbenitez <asieldbenitez@gmail.com>
2025-12-16 20:30:50 +0100

    update scripts

--- scripts/undo_rebrand.sh
+++ scripts/undo_rebrand.sh
@@ -6,6 +6,7 @@ find ./src/main/assets/help/ -type f -name '*.html' | xargs sed -i 's/ArcaneChat
 find ./src/ -type f -name 'strings.xml' | xargs sed -i 's/github.com\/ArcaneChat/get.delta.chat/g'
 find ./src/ -type f -name 'strings.xml' | xargs sed -i 's/arcanechat.me\/#contribute/delta.chat\/donate/g'
 find ./src/ -type f -name 'strings.xml' | xargs sed -i 's/ArcaneChat/Delta Chat/g'
+find ./src/ -type f -name 'strings.xml' | xargs sed -i 's/💜/❤️/g'
 
 # don't revert the app name
 sed -i 's/>Delta Chat</>ArcaneChat</g' ./src/main/res/values/strings.xml
