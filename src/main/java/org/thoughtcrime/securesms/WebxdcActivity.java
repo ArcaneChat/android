3550646e0814b13a702dc9b162082ce617725cbd
adbenitez <asieldbenitez@gmail.com>
2026-09-04 00:22:48 +0200

    apply spotless

--- src/main/java/org/thoughtcrime/securesms/WebxdcActivity.java
+++ src/main/java/org/thoughtcrime/securesms/WebxdcActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
